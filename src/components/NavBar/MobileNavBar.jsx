a9f3a70a1c06d2cff8e7b0dfb552634ec44f62e3
Sakhee89 <kung_fu_fighting_monkey@hotmail.com>
2024-03-17 04:20:21 +0000

    fix css

--- src/components/NavBar/MobileNavBar.jsx
+++ src/components/NavBar/MobileNavBar.jsx
@@ -9,14 +9,12 @@ export function MobileNavBar({ isOpen, toggleMenu }) {
   return (
     <>
       <div
-        className={`w-screen h-screen block bg-black text-white bg-opacity-30 fixed left-0 top-0 z-[999] opacity-0 custom-box-shadow transition-all duration-300 ease-in-out translate-x-[-100vw] md:hidden ${
-          isOpen ? "translate-x-0 opacity-100" : ""
-        }`}
+        className={`mobile-menu ${isOpen ? "active" : ""}`}
         onClick={toggleMenu}
       >
-        <div className="w-[60vw] h-screen bg-[#342864] p-8 active:opacity-[1] active:translate-x-0">
+        <div className="w-[45vw] h-screen bg-[#342864] p-8 active:opacity-[1] active:translate-x-0">
           <h1 className="mb-12">Portfolio</h1>
-          <ul className="flex flex-col gap-8 list-none ml-[-2rem]">
+          <ul className="flex flex-col gap-8 list-none ml-[-1.5rem]">
             <li className="mx-6">
               <a className="a-style">Home</a>
             </li>
