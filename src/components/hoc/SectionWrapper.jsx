55e716798d289f934dd1b5275a985f6841809c8c
Sakhee89 <kung_fu_fighting_monkey@hotmail.com>
2026-07-12 01:26:26 +0100

    Fix projects section visibility on mobile

--- src/components/hoc/SectionWrapper.jsx
+++ src/components/hoc/SectionWrapper.jsx
@@ -7,8 +7,9 @@ export function SectionWrapper(Component, idName) {
       <motion.section
         variants={staggerContainer()}
         initial="hidden"
-        whileInView="show"
-        viewport={{ once: true, amount: 0.25 }}
+        animate="show"
+        // whileInView="show"
+        // viewport={{ once: true, amount: 0.25 }}
         className="sm:px-16 px-6 sm:py-16 py-10 max-w-7xl mx-auto relative z-0"
       >
         <span className="mt-[-100px] pb-[100px] block" id={idName}>
